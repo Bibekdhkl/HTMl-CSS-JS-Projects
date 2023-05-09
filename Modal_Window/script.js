1ceae65e98faed7cffa77bc68ecb16cbdc3b8911
bibekdhkl <bibekdh2016@gmail.com>
2023-05-09 23:58:37 +0545

    Modal Window UI updated

--- Modal_Window/script.js
+++ Modal_Window/script.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const modal = document.querySelector('.modal-window');
+const btnShowModal = document.querySelectorAll('.show-modal')
+const btnCloseModal = document.querySelector('.close-modal')

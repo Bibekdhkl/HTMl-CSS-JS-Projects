8c9da0b5b74b4cc88803264346e2e04168253a65
bibekdhkl <bibekdh2016@gmail.com>
2023-05-14 23:45:03 +0545

    :tada:Issue on Button Click

--- Modal_Window/script.js
+++ Modal_Window/script.js
@@ -1,5 +1,23 @@
-'use strict';
+"use strict";
 
-const modal = document.querySelector('.modal-window');
-const btnShowModal = document.querySelectorAll('.show-modal')
-const btnCloseModal = document.querySelector('.close-modal')
+// we are storing all the model
+const modal = document.querySelector(".modal-window");
+const btnShowModal = document.querySelectorAll(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+// btnCloseModal.addEventListener("click", function() => {});
+
+for (let i = 0; i <= btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", function () {
+    console.log("Buttton Clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+
+btnCloseModal.addEventListener("click", function () {
+  console.log("we are closing");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});

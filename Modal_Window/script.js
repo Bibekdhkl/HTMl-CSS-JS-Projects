098ed1a2b3f24ab7084f07d5010d236280bae1dd
bibekdhkl <bibekdh2016@gmail.com>
2023-05-23 10:04:12 +0545

    Completed Modal Window

--- Modal_Window/script.js
+++ Modal_Window/script.js
@@ -6,9 +6,24 @@ const btnShowModal = document.querySelectorAll(".show-modal");
 const btnCloseModal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
+//if we keep this weird part at last section of this page then it won't work but I don't know why haha
+// it's because of using array operator as it returns null operator
+
+const openModal = () => {
+  console.log("Buttton Clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// for (let i = 0; i <= btnShowModal.length; i++) {
+//   btnShowModal[i].addEventListener("click", openModal);
+// }
+btnShowModal.forEach((element) => {
+  element.addEventListener("click", openModal);
+});
 // weird part of js
 
-const closeModal = function () {
+const closeModal = () => {
   console.log("we are closing");
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
@@ -18,14 +33,13 @@ btnCloseModal.addEventListener("click", closeModal);
 //we don't write closeModal() over here as it gets executed without click event
 overlay.addEventListener("click", closeModal);
 //end here weird part
-//if we keep this weird part at last section of this page then it won't work but I don't know why haha
 
-const openModal = function () {
-  console.log("Buttton Clicked");
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-for (let i = 0; i <= btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener("click", openModal);
-}
+//close the model when Esc is placed
+document.addEventListener("keydown", (e) => {
+  console.log("Keydown button pressed");
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
